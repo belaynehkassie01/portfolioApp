9aad379a7220a21751ee0ac93a05db89e9a45269
belaynehkassie01 <belaynehkassie01@gmail.com>
2026-07-17 08:33:11 -0800

    chnage cv and resume123

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,7 +25,7 @@ import {
 import '../assets/styles/pages/About.css';
 import AboutImage from '../assets/images/About.jpg';
 import About1 from '../assets/images/about1.jpg';
-// Import the CV file
+// Import the CV file  
 import MyCV from '../assets/Files/Belayneh_kassie_CV.pdf';
 
 
